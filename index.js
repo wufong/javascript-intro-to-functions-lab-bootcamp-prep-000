08abb6f563e8ae0517430b731f5cab3a9f52327f
Mike Walker <jerry.kowal@gmail.com>
2018-05-19 00:25:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function whisper(string) {
 function logShout(string) {
   console.log(string.toUpperCase())
 }
-function logWhisper() {
+function logWhisper(string) {
   console.log(string.toLowerCase())
 }
